1188d809abeabdd469838446315b0a96445fe37d
DESKTOP-02ARFGQ\ahmed <ahmed.ghanafer@gmail.com>
2025-08-19 17:07:30 +0200

    Fixed Build Errors

--- Minishop.Api/Program.cs
+++ Minishop.Api/Program.cs
@@ -21,17 +21,15 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-app.UseCors();
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+
 
 app.MapGet("/health", () =>
     {
@@ -41,3 +39,4 @@ app.MapGet("/health", () =>
     });
 
 app.Run();
+public partial class Program { }
